613280ab40da911c2dd7fbd28b172971d5d0bbf4
Hakarp <95172141+Hakarp@users.noreply.github.com>
2021-12-21 22:10:36 +0100

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,376 @@
+fx_version 'cerulean'
+
+-------alamo-------
+
+files {
+	'alamo/common/carcols.meta',
+	'alamo/common/carvariations.meta',
+	'alamo/common/dlctext.meta',
+	'alamo/common/handling.meta',
+	'alamo/common/vehicles.meta',
+	'alamo/common/*.meta'
+}
+data_file 'DLCTEXT_FILE' 'alamo/common/dlctext.meta'
+data_file 'HANDLING_FILE' 'alamo/common/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'alamo/common/vehicles.meta'
+data_file 'CARCOLS_FILE' 'alamo/common/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'alamo/common/carvariations.meta'
+
+
+-------BRAVO-------
+
+files {
+	'BRAVO/common/carcols.meta',
+	'BRAVO/common/carvariations.meta',
+	'BRAVO/common/dlctext.meta',
+	'BRAVO/common/handling.meta',
+	'BRAVO/common/vehicles.meta',
+	'BRAVO/common/*.meta'
+}
+data_file 'DLCTEXT_FILE' 'BRAVO/common/dlctext.meta'
+data_file 'HANDLING_FILE' 'BRAVO/common/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'BRAVO/common/vehicles.meta'
+data_file 'CARCOLS_FILE' 'BRAVO/common/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'BRAVO/common/carvariations.meta'
+
+
+-------pol718-------
+
+files {
+	'pol718/common/carcols.meta',
+	'pol718/common/carvariations.meta',
+	'pol718/common/dlctext.meta',
+	'pol718/common/handling.meta',
+	'pol718/common/vehicles.meta',
+	'pol718/common/*.meta'
+}
+data_file 'DLCTEXT_FILE' 'pol718/common/dlctext.meta'
+data_file 'HANDLING_FILE' 'pol718/common/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'pol718/common/vehicles.meta'
+data_file 'CARCOLS_FILE' 'pol718/common/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'pol718/common/carvariations.meta'
+
+
+-------ghispo2-------
+
+files {
+	'ghispo2/common/carcols.meta',
+	'ghispo2/common/carvariations.meta',
+	'ghispo2/common/dlctext.meta',
+	'ghispo2/common/handling.meta',
+	'ghispo2/common/vehicles.meta',
+	'ghispo2/common/*.meta'
+}
+data_file 'DLCTEXT_FILE' 'ghispo2/common/dlctext.meta'
+data_file 'HANDLING_FILE' 'ghispo2/common/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'ghispo2/common/vehicles.meta'
+data_file 'CARCOLS_FILE' 'ghispo2/common/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'ghispo2/common/carvariations.meta'
+
+
+-------dodge-------
+fx_version 'cerulean'
+game 'gta5'
+files {
+	'common/*.meta'
+}
+
+
+-------lspd3-------
+
+files {
+	'lspd3/common/carcols.meta',
+	'lspd3/common/carvariations.meta',
+	'lspd3/common/dlctext.meta',
+	'lspd3/common/vehicles.meta',
+	'lspd3/common/*.meta'
+}
+data_file 'DLCTEXT_FILE' 'vcommon/dlctext.meta'
+data_file 'VEHICLE_METADATA_FILE' 'lspd3/common/vehicles.meta'
+data_file 'CARCOLS_FILE' 'lspd3/common/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'lspd3/common/carvariations.meta'
+
+-------panto-------
+
+files {
+	'common/*.meta'
+}
+
+-------pturismo-------
+
+files {
+	'pturismo/common/carcols.meta',
+	'pturismo/common/carvariations.meta',
+	'pturismo/common/dlctext.meta',
+	'pturismo/common/handling.meta',
+	'pturismo/common/vehicles.meta',
+	'pturismo/common/*.meta'
+}
+data_file 'DLCTEXT_FILE' 'pturismo/common/dlctext.meta'
+data_file 'HANDLING_FILE' 'pturismo/common/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'pturismo/common/vehicles.meta'
+data_file 'CARCOLS_FILE' 'pturismo/common/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'pturismo/common/carvariations.meta'
+
+------POLRAIDEN--------
+
+files {
+	'POLRAIDEN/common/carcols.meta',
+	'POLRAIDEN/common/carvariations.meta',
+	'POLRAIDEN/common/dlctext.meta',
+	'POLRAIDEN/common/handling.meta',
+	'POLRAIDEN/common/vehiclelayouts.meta',
+	'POLRAIDEN/common/vehicles.meta',
+	'POLRAIDEN/common/*.meta'
+}
+data_file 'DLCTEXT_FILE' 'POLRAIDEN/common/dlctext.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'POLRAIDEN/common/vehiclelayouts.meta'
+data_file 'HANDLING_FILE' 'POLRAIDEN/common/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'POLRAIDEN/common/vehicles.meta'
+data_file 'CARCOLS_FILE' 'POLRAIDEN/common/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'POLRAIDEN/common/carvariations.meta'
+
+
+------polamggtr--------
+
+files {
+	'polamggtr/common/carcols.meta',
+	'polamggtr/common/carvariations.meta',
+	'polamggtr/common/dlctext.meta',
+	'polamggtr/common/handling.meta',
+	'polamggtr/common/vehicles.meta',
+	'polamggtr/common/*.meta'
+}
+data_file 'DLCTEXT_FILE' 'polamggtr/common/dlctext.meta'
+data_file 'HANDLING_FILE' 'polamggtr/common/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'polamggtr/common/vehicles.meta'
+data_file 'CARCOLS_FILE' 'polamggtr/common/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'polamggtr/common/carvariations.meta'
+
+
+------polaventa--------
+
+files {
+	'polaventa/common/carcols.meta',
+	'polaventa/common/carvariations.meta',
+	'polaventa/common/dlctext.meta',
+	'polaventa/common/handling.meta',
+	'polaventa/common/vehicles.meta',
+	'polaventa/common/*.meta'
+}
+data_file 'DLCTEXT_FILE' 'polaventa/common/dlctext.meta'
+data_file 'HANDLING_FILE' 'polaventa/common/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'polaventa/common/vehicles.meta'
+data_file 'CARCOLS_FILE' 'polaventa/common/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'polaventa/common/carvariations.meta'
+
+
+------polp1--------
+
+files {
+	'polp1/common/carcols.meta',
+	'polp1/common/carvariations.meta',
+	'polp1/common/dlctext.meta',
+	'polp1/common/handling.meta',
+	'polp1/common/vehicles.meta',
+	'polp1/common/*.meta'
+}
+data_file 'DLCTEXT_FILE' 'polp1/common/dlctext.meta'
+data_file 'HANDLING_FILE' 'polp1/common/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'polp1/common/vehicles.meta'
+data_file 'CARCOLS_FILE' 'polp1/common/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'polp1/common/carvariations.meta'
+
+
+------policefelon--------
+
+files {
+	'policefelon/common/carcols.meta',
+	'policefelon/common/carvariations.meta',
+	'policefelon/common/handling.meta',
+	'policefelon/common/vehicles.meta',
+	'policefelon/common/*.meta'
+}
+data_file 'HANDLING_FILE' 'policefelon/common/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'policefelon/common/vehicles.meta'
+data_file 'CARCOLS_FILE' 'policefelon/common/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'policefelon/common/carvariations.meta'
+
+
+------SUVP--------
+
+files {
+	'SUVP/common/*.meta'
+}
+
+------policegt350r--------
+
+files {
+	'policegt350r/common/carcols.meta',
+	'policegt350r/common/carvariations.meta',
+	'policegt350r/common/dlctext.meta',
+	'policegt350r/common/handling.meta',
+	'policegt350r/common/vehicles.meta',
+	'policegt350r/common/*.meta'
+}
+data_file 'DLCTEXT_FILE' 'policegt350r/common/dlctext.meta'
+data_file 'HANDLING_FILE' 'policegt350r/common/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'policegt350r/common/vehicles.meta'
+data_file 'CARCOLS_FILE' 'policegt350r/common/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'policegt350r/common/carvariations.meta'
+
+
+------polcaracara--------
+
+files {
+	'polcaracara/common/carcols.meta',
+	'polcaracara/common/carvariations.meta',
+	'polcaracara/common/dlctext.meta',
+	'polcaracara/common/handling.meta',
+	'polcaracara/common/vehicles.meta',
+	'polcaracara/common/*.meta'
+}
+data_file 'DLCTEXT_FILE' 'polcaracara/common/dlctext.meta'
+data_file 'HANDLING_FILE' 'polcaracara/common/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'polcaracara/common/vehicles.meta'
+data_file 'CARCOLS_FILE' 'polcaracara/common/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'polcaracara/common/carvariations.meta'
+
+
+------lspdheli--------
+
+files {
+	'lspdheli/common/*.meta'
+}
+
+
+------Buzzard--------
+
+fx_version 'cerulean'
+game 'gta5'
+files {
+	'common/*.meta'
+}
+
+
+
+------pcscout--------
+
+fx_version 'cerulean'
+game 'gta5'
+files {
+	'pscout/common/carcols.meta',
+	'pscout/common/carvariations.meta',
+	'pscout/common/dlctext.meta',
+	'pscout/common/handling.meta',
+	'pscout/ommon/vehicles.meta',
+	'pscout/common/*.meta'
+}
+data_file 'DLCTEXT_FILE' 'pscout/common/dlctext.meta'
+data_file 'HANDLING_FILE' 'pscout/common/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'pscout/common/vehicles.meta'
+data_file 'CARCOLS_FILE' 'pscout/common/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'pscout/common/carvariations.meta'
+
+
+
+------mini voiture--------
+
+fx_version 'cerulean'
+game 'gta5'
+files {
+	'blazerrota/common/carcols.meta',
+	'blazerrota/common/carvariations.meta',
+	'blazerrota/common/handling.meta',
+	'blazerrota/common/vehicles.meta',
+	'blazerrota/common/*.meta'
+}
+data_file 'HANDLING_FILE' 'blazerrota/common/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'blazerrota/common/vehicles.meta'
+data_file 'CARCOLS_FILE' 'blazerrota/common/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'blazerrota/common/carvariations.meta'
+
+------crown--------
+
+fx_version 'cerulean'
+game 'gta5'
+files {
+	'common/*.meta'
+}
+
+------velo--------
+
+fx_version 'cerulean'
+game 'gta5'
+files {
+	'common/*.meta'
+}
+
+------cap--------
+
+
+
+------buffalo--------
+fx_version 'cerulean'
+game 'gta5'
+files {
+	'pol2b/common/carcols.meta',
+	'pol2b/common/carvariations.meta',
+	'pol2b/common/dlctext.meta',
+	'pol2b/common/vehicles.meta',
+	'pol2b/common/*.meta'
+}
+data_file 'DLCTEXT_FILE' 'pol2b/common/dlctext.meta'
+data_file 'VEHICLE_METADATA_FILE' 'pol2b/common/vehicles.meta'
+data_file 'CARCOLS_FILE' 'pol2b/common/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'pol2b/common/carvariations.meta'
+
+
+------scout--------
+
+fx_version 'cerulean'
+game 'gta5'
+files {
+	'polscout/common/carcols.meta',
+	'polscout/common/carvariations.meta',
+	'polscout/common/dlctext.meta',
+	'polscout/common/handling.meta',
+	'polscout/common/shop_vehicle.meta',
+	'polscout/common/vehicles.meta',
+	'polscout/common/*.meta'
+}
+data_file 'DLCTEXT_FILE' 'polscout/common/dlctext.meta'
+data_file 'HANDLING_FILE' 'polscout/common/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'polscout/common/vehicles.meta'
+data_file 'CARCOLS_FILE' 'polscout/common/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'polscout/common/carvariations.meta'
+data_file 'VEHICLE_SHOP_FILE' 'polscout/common/shop_vehicle.meta'
+
+
+
+
+
+
+fx_version 'cerulean'
+game 'gta5'
+files {
+	'common/*.meta'
+}
+
+
+
+fx_version 'cerulean'
+game 'gta5'
+files {
+	'drafterintcept/common/carcols.meta',
+	'drafterintcept/common/carvariations.meta',
+	'drafterintcept/common/dlctext.meta',
+	'drafterintcept/common/handling.meta',
+	'drafterintcept/common/vehicles.meta',
+	'drafterintcept/common/*.meta'
+}
+data_file 'DLCTEXT_FILE' 'drafterintcept/common/dlctext.meta'
+data_file 'HANDLING_FILE' 'drafterintcept/common/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'drafterintcept/common/vehicles.meta'
+data_file 'CARCOLS_FILE' 'drafterintcept/common/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'drafterintcept/common/carvariations.meta'
